f7855883b7b52888ac8d5ca1ab69a77d9b5a3c0a
Jewgenij Larkin <jewglark@Jewgenijs-MacBook-Pro.local>
2024-01-29 11:31:12 +0100

    fixed bot config

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
+const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
 
 const token = '6409534713:AAEnHxLbb7OF_JC0nY5dHTN4Ff4vrkUEQtI';
+const webhookUrl = 'https://ban-master-2000-bot.vercel.app/';
+
 const bot = new TelegramBot(token);
 
-const webhookUrl = 'https://project-w-alpha.vercel.app/';
-const port = 3002;
-
 const app = express();
+
+app.use(bodyParser.json());
 
 app.post(`/webhook/${token}`, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Webhook server is listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 bot.setWebHook(`${webhookUrl}/webhook/${token}`);
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  console.log(msg, chatId);
 
   if (msg.text) {
     bot.sendMessage(chatId, `Вы отправили: ${msg.text}`);
